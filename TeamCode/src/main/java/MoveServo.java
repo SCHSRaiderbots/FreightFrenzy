c82cc02f91bdc8022fd8d72510dfaa7c78ab05d0
ShreyaVora14 <shreya.sm.vora@gmail.com>
2021-10-18 14:02:55 -0700

    servo test

---
+++ TeamCode/src/main/java/MoveServo.java
@@ -0,0 +1,28 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Linear;
+
+@TeleOp
+public class MoveServo extends OpMode {
+    Servo myServo;
+    public double open = 0.4;
+    public double close = 0.0;
+
+
+    @Override
+    public void init() {
+        myServo = hardwareMap.get(Servo.class, "myServo");
+        myServo.setPosition(close);
+    }
+    public void loop(){
+        if (gamepad1.a){
+            myServo.setPosition(open);
+        }
+        if (gamepad1.b){
+            myServo.setPosition(close);
+        }
+
+    }
+}
