3bf51959325db0b65d6fd508ff512917f3c3c9e1
GLRoylance <GLRoylance@gmail.com>
2021-11-08 17:27:34 -0800

    DriveSimple as an OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveSimple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveSimple.java
@@ -1,31 +1,58 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name="DriveSimple", group="simple group")
-public class DriveSimple extends LinearOpMode {
+import java.util.Locale;
+
+@TeleOp(name="DriveSimple", group="CodeDev")
+public class DriveSimple extends OpMode {
     DcMotorEx motorLeft;
     DcMotorEx motorRight;
 
     @Override
-    public void runOpMode() {
-        motorLeft = hardwareMap.get(DcMotorEx.class, "motorLeft");
-        motorRight = hardwareMap.get(DcMotorEx.class, "motorRight");
-        // value for conversion
-        final double power = 1.0;
+    public void init() {
+        // get the motors
+        motorLeft = hardwareMap.get(DcMotorEx.class, "leftMotor");
+        motorRight = hardwareMap.get(DcMotorEx.class, "rightMotor");
 
+        // set the motor directions
         motorLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        waitForStart();
+        telemetry.addData("Drive motors", "initialized");
+    }
 
-        while (opModeIsActive()){
-            motorLeft.setPower(gamepad1.left_stick_y * power);
-            motorRight.setPower(gamepad1.right_stick_y * power);
-        }
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void loop() {
+
+        // value for conversion
+        final double power = 1.0;
+
+        // get the operator commands
+        double powerLeft = gamepad1.left_stick_y * power;
+        double powerRight = gamepad1.right_stick_y * power;
+
+        // set the motor power levels
+        motorLeft.setPower(powerLeft);
+        motorRight.setPower(powerRight);
+
+        // report the power levels
+        telemetry.addData("Drive motors", String.format((Locale)null, "%.03f %.03f", powerLeft, powerRight));
+    }
+
+    @Override
+    public void stop() {
     }
 
 }
