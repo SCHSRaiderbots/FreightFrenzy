6ef8aa8d96dce2da1973dae368758178c0e86dbb
ShreyaVora14 <shreya.sm.vora@gmail.com>
2022-01-10 15:10:57 -0800

    Anshul test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarouselTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarouselTest.java
@@ -1,22 +1,22 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-
-@TeleOp(name = "Carousel Test", group = "CodeDev")
-public class CarouselTest extends LinearOpMode {
-    public DcMotorEx motorCarousel;
-
-    @Override
-    public void runOpMode() {
-        motorCarousel = hardwareMap.get(DcMotorEx.class, "motorCarousel");
-
-        waitForStart();
-        while (opModeIsActive()) {
-            telemetry.addData("motor", motorCarousel.getVelocity());
-            motorCarousel.setVelocity(2288 * gamepad1.right_stick_y);
-            telemetry.update();
-        }
-    }
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.DcMotorEx;
+//
+//@TeleOp(name = "Carousel Test", group = "CodeDev")
+//public class CarouselTest extends LinearOpMode {
+//    public DcMotorEx motorCarousel;
+//
+//    @Override
+//    public void runOpMode() {
+//        motorCarousel = hardwareMap.get(DcMotorEx.class, "motorCarousel");
+//
+//        waitForStart();
+//        while (opModeIsActive()) {
+//            telemetry.addData("motor", motorCarousel.getVelocity());
+//            motorCarousel.setVelocity(2288 * gamepad1.right_stick_y);
+//            telemetry.update();
+//        }
+//    }
+//}
