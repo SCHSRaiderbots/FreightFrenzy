c1960d744c1c8f28193873a844757cb6fbca8b10
pjmsvvfamily <shreyans481@gmail.com>
2022-01-10 14:02:51 -0800

    Carousel work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Carousel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Carousel.java
@@ -34,6 +34,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -51,37 +54,27 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Carousel", group="Linear Opmode")
-public class Carousel extends LinearOpMode {
+public class Carousel{
 
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    //private DcMotor leftDrive = null;
-    //private DcMotor rightDrive = null;
-
-    DcMotor carouselMotor;
+    DcMotorEx carouselMotor;
     double power = 0.5;
 
 
     @Override
-    public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        carouselMotor  = hardwareMap.get(DcMotor.class, "Carousel");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
+    public void init(HardwareMap hardwareMap) {
 
 
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        runtime.reset();
+        carouselMotor  = hardwareMap.get(DcMotorEx.class, "Carousel");
 
-        // run until the end of the match (driver presses STOP)
+        carouselMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        carouselMotor.setVelocity(0);
+        carouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        carouselMotor.setPower(power);
 
     }
+
+    public void spin(double velocity){
+        carouselMotor.setVelocity(velocity);
+    }
+
 }
