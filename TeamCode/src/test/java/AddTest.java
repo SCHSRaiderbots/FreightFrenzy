ad727bf7d9792ec6fdd0901b74158b1cccdfa7ca
GLRoylance <GLRoylance@gmail.com>
2023-05-02 17:47:39 -0700

    figured out package issue

--- TeamCode/src/test/java/AddTest.java
+++ TeamCode/src/test/java/AddTest.java
@@ -1,15 +1,18 @@
-package org.firstinspires.ftc.teamcode;
+// package org.firstinspires.ftc.teamcode;
 
 // TODO: package above is screwy
+// the package name must follow the directory structure....
 
 // I got JUnit 4 to work...
 //   Added to build.gradle (TeamCode):
 //      testImplementation 'junit:junit:4.12'
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 // but not JUnit Jupiter...
 // import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // import org.junit.jupiter.api.Test;
 
 // for more involved tests, see
